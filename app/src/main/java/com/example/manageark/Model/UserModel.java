1e6574bc62a7e73f43f76386cfb68c43ae3981c7
ritik ranjan <rritik772@outlook.com>
2021-11-22 17:18:54 +0530

    Firebase login signup refactored
    - ENUMS/Status.java // for Error status in the application
    - Model/firebaseMessage // for returning from the async task

--- app/src/main/java/com/example/manageark/Model/UserModel.java
+++ app/src/main/java/com/example/manageark/Model/UserModel.java
@@ -1,13 +1,15 @@
 package com.example.manageark.Model;
 
 public class UserModel {
+    private String uid;
     private String fullName;
     private String email;
     private String uniqueId;
     private String university;
     private String photoUrl;
 
-    public UserModel(String fullName, String email, String uniqueId, String university, String photoUrl) {
+    public UserModel(String uid, String fullName, String email, String uniqueId, String university, String photoUrl) {
+        this.uid = uid;
         this.fullName = fullName;
         this.email = email;
         this.uniqueId = uniqueId;
@@ -15,10 +17,12 @@ public class UserModel {
         this.photoUrl = photoUrl;
     }
 
+    public void setUID(String uid) { this.uid = uid; }
+    public String getUID() { return this.uid; }
+
     public String getFullName() {
         return fullName;
     }
-
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
@@ -26,7 +30,6 @@ public class UserModel {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -34,7 +37,6 @@ public class UserModel {
     public String getUniqueId() {
         return uniqueId;
     }
-
     public void setUniqueId(String uniqueId) {
         this.uniqueId = uniqueId;
     }
@@ -42,7 +44,6 @@ public class UserModel {
     public String getUniversity() {
         return university;
     }
-
     public void setUniversity(String university) {
         this.university = university;
     }
@@ -50,7 +51,6 @@ public class UserModel {
     public String getPhotoUrl() {
         return photoUrl;
     }
-
     public void setPhotoUrl(String photoUrl) {
         this.photoUrl = photoUrl;
     }
---
+++ app/src/main/java/com/example/manageark/firestoreUtil/userUtilsStore.java
@@ -0,0 +1,43 @@
+package com.example.manageark.firestoreUtil;
+
+import androidx.annotation.NonNull;
+
+import com.example.manageark.ENUMS.Status;
+import com.example.manageark.Model.UserModel;
+import com.example.manageark.Model.firebaseMessage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class userUtilsStore {
+    private final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+    public firebaseMessage postSignup(UserModel userModel){
+        final firebaseMessage[] message = {
+                new firebaseMessage(Status.Successful, "User Created Successfully")
+        };
+
+        firestore
+                .collection("users")
+                .document(userModel.getUID())
+                .set(userModel)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        message[0] = new firebaseMessage(
+                                Status.Successful, "User Created Successfully"
+                        );
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        message[0] = new firebaseMessage(
+                                Status.Unsuccessful, "Something went wrong."
+                        );
+                    }
+                });
+
+        return  message[0];
+    }
+}
